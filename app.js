7e9e9ccc20253600827823d3644ea37ca273d43a
Woohyeok Kim, Aaron <woohyeok.kim@rakuten.com>
2018-12-10 17:11:39 +0900

    Express - how to express web page

--- app.js
+++ app.js
@@ -2,6 +2,36 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
+app.get('/', function(req, res){
+  res.send('Hello woosyumes home!');
+});
+
+app.get('/dynamic', function(req, res){
+var lis = '';
+for (var i = 0; i < 5; i++) {
+  lis = lis + '<li>coding</li>';
+}
+
+var time = Date();
+var output = `
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Hello Dynamic! </title>
+  </head>
+  <body>
+    Hello Dynamic!
+    <ul>
+    ${lis}
+    </ul>
+    ${time}
+  </body>
+</html>
+`
+  res.send(output);
+})
+
 app.get('/route', function(req, res){
   res.send('Hello Router, <img src="/woohyeok.png"/>');
 });
@@ -13,5 +43,3 @@ app.get('/login', function(req, res){
 app.listen(3000, function(){
   console.log('Connected 3000 port!');
 });
-
-
