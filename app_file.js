0050146a75f30cd85f7bb79b2c90881545e12e80
Woohyeok Kim, Aaron <woohyeok.kim@rakuten.com>
2018-12-11 19:00:18 +0900

    ReadDir and ReadFile

--- app_file.js
+++ app_file.js
@@ -17,15 +17,33 @@ app.get('/topic/new', function(req, res) {
   res.render('new'); // Use render method when uses template engine.
 });
 
-app.post('/topic', function(req, res) {
-  var title = req.body.title;
-  var description = req.body.description;
-  fs.writeFile('data/' + title, description, function(err) {
+app.get('/topic/:id', function(req, res) {
+  var id = req.params.id;
+
+  fs.readdir('data', function(err, files) {
     if(err) {
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
 
-    res.send('Success');
+    fs.readFile('data/' + id, 'utf8', function(err, data) {
+      if(err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+      }
+
+      res.render('view', {topics: files, title: id, description: data})
+    });
+  });
+});
+
+app.get('/topic', function(req, res) {
+  fs.readdir('data', function(err, files) {
+    if(err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+
+    res.render('view', {topics:files});
   });
 });
