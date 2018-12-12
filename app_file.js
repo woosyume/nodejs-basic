1059944042ecab161077e99514869e7125fe48c9
Woohyeok Kim, Aaron <woohyeok.kim@rakuten.com>
2018-12-13 08:24:08 +0900

    code refactoring

--- app_file.js
+++ app_file.js
@@ -14,36 +14,48 @@ app.listen(3001, function() {
 });
 
 app.get('/topic/new', function(req, res) {
-  res.render('new'); // Use render method when uses template engine.
-});
-
-app.get('/topic/:id', function(req, res) {
-  var id = req.params.id;
-
   fs.readdir('data', function(err, files) {
     if(err) {
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
-
-    fs.readFile('data/' + id, 'utf8', function(err, data) {
-      if(err) {
-        console.log(err);
-        res.status(500).send('Internal Server Error');
-      }
-
-      res.render('view', {topics: files, title: id, description: data})
-    });
+    res.render('new', {topics: files}); // Use render method when uses template engine.
   });
 });
 
-app.get('/topic', function(req, res) {
+// Input router address to array
+app.get(['/topic', '/topic/:id'], function(req, res) {
   fs.readdir('data', function(err, files) {
     if(err) {
       console.log(err);
       res.status(500).send('Internal Server Error');
     }
+    var id = req.params.id;
 
-    res.render('view', {topics:files});
+    // When request has id value
+    if (id) {
+      fs.readFile('data/' + id, 'utf8', function(err, data) {
+        if(err) {
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        }
+        res.render('view', {topics: files, title: id, description: data})
+      });
+    } else {
+      res.render('view', {topics:files, title:'Welcome', description: 'Hello, Javascript for Server.'});
+    }
+  });
+});
+
+app.post('/topic', function(req, res) {
+  var title = req.body.title;
+  var description = req.body.description;
+
+  fs.writeFile('data/' + title, description, function(err) {
+    if(err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.redirect('/topic/' + title);
   });
 });
